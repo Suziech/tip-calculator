1b428d0ebbb17c69b8ba003ea26cd812aab4a329
최슬기 <choisk09@gmail.com>
2026-02-13 14:55:36 -0500

    feat: add 10% and 18% tip buttons, tweak styling

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const billInput = document.getElementById("bill");
+const btn10 = document.getElementById("10%");
 const btn15 = document.getElementById("15%");
+const btn18 = document.getElementById("18%");
 const btn20 = document.getElementById("20%");
 const result = document.getElementById("result");
 
@@ -12,5 +14,7 @@ function calculateTip(percentage) {
     return billAmount;
 }
 
+btn10.addEventListener("click", () => { calculateTip(10) });
 btn15.addEventListener("click", () => { calculateTip(15) });
+btn18.addEventListener("click", () => { calculateTip(18) });
 btn20.addEventListener("click", () => { calculateTip(20) });
